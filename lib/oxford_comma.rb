79eada7765f849abde04288fe1f147c5f27e7963
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-22 02:37:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+# require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join " and " 
+  elsif array.length >= 3
+    array[-1].insert(0,"and ")
+      array.join ", "
+    # binding.pry
+  end
 end
+
