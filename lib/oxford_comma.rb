dddc255b566fc92e593edb74d759a5283b3a4b42
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-18 09:01:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,50 @@
 # require 'pry'
 def oxford_comma(array)
   if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join " and " 
+    array.first
+  elsif array.length == 2 
+    array.first + " and " + array.last
   elsif array.length >= 3
-    array[-1].insert(0,"and ")
-      array.join ", "
-    # binding.pry
+  all_but_last = array.length - 1
+  array[0...all_but_last].join(", ") + ", and " + array.last
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# def oxford_comma(array)
+#   if array.length == 1
+#     array.join
+#   elsif array.length == 2
+#     array.join " and " 
+#   elsif array.length >= 3
+#     array[-1].insert(0,"and ")
+#       array.join ", "
+#     # binding.pry
+#   end
+# end
 
